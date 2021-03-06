ddff216cbeff17ee05f909b6ebb962b2ed1a23fe
Alex <oneilla23@gmail.com>
2021-03-05 19:20:30 -0500

    Added functions into the argparse logic

--- list_content.py
+++ list_content.py
@@ -11,6 +11,8 @@ import logging
 import os
 import shutil
 import string
+import time
+from pathlib import Path
 
 # delete 'filename = path' to show the log in the console and not save to a file
 logging.basicConfig(level=logging.DEBUG,
@@ -50,7 +52,6 @@ def list_drives(drive):
             dirnum = 0
             filenum = 0
 
-            # todo do we need to sum the immediate directories and files or all sub files and directories?
             for lists in os.listdir(path):
                 sub_path = os.path.join(path, lists)
                 # print(sub_path)
@@ -60,6 +61,88 @@ def list_drives(drive):
                     dirnum = dirnum + 1
             print('The total number of directories is:', dirnum)
             print('The total number of files is:', filenum)
+
+
+def get_folder_info(DIR):
+    total_size = sum(f.stat().st_size for f in Path(DIR).glob('**/*') if f.is_file())
+    for item in os.listdir(DIR):
+        itempath = os.path.join(DIR, item)
+        if os.path.isfile(itempath):
+            continue
+
+        elif os.path.isdir(itempath):
+            filenum = len(os.listdir(itempath))
+            root_directory = Path(itempath)
+            a = sum(f.stat().st_size for f in root_directory.glob('**/*') if f.is_file())
+            print('folder: {}\tfile num: {}\tfolder size: {}'.format(itempath, filenum, sizeConvert(a)))
+
+
+# todo DO WE WANT FULL FOLDER RECURSION ON THIS? OR JUST LEVEL 0?
+def get_all_files(DIR):
+    for item in os.listdir(DIR):
+        itempath = os.path.join(DIR, item)
+        if os.path.isfile(itempath):
+            filename = item
+            filesize = os.stat(itempath).st_size
+            filetime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.stat(itempath).st_ctime))
+            print('filename: {}, filesize: {}, filetime: {}'.format(filename, sizeConvert(filesize), filetime))
+
+        elif os.path.isdir(itempath):
+            a = get_all_files(itempath)
+            if None != a:
+                print(a)
+
+
+# todo DO WE WANT FULL FOLDER RECURSION ON THIS? OR JUST LEVEL 0?
+def get_all_types(path):
+    py_file_num = 0
+    py_file_size = 0
+    ipynb_file_num = 0
+    ipynb_file_size = 0
+    exe_file_num = 0
+    exe_file_size = 0
+    txt_file_num = 0
+    txt_file_size = 0
+    csv_file_num = 0
+    csv_file_size = 0
+    pdf_file_num = 0
+    pdf_file_size = 0
+    other_file_num = 0
+    other_file_size = 0
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            filepath = os.path.join(root, file)
+            if os.path.splitext(file)[-1] == '.py':
+                py_file_num += 1
+                py_file_size += os.stat(filepath).st_size
+            elif os.path.splitext(file)[-1] == '.ipynb':
+                ipynb_file_num += 1
+                ipynb_file_size += os.stat(filepath).st_size
+            elif os.path.splitext(file)[-1] == '.exe':
+                exe_file_num += 1
+                exe_file_size += os.stat(filepath).st_size
+            elif os.path.splitext(file)[-1] == '.txt':
+                txt_file_num += 1
+                txt_file_size += os.stat(filepath).st_size
+            elif os.path.splitext(file)[-1] == '.csv':
+                csv_file_num += 1
+                csv_file_size += os.stat(filepath).st_size
+            elif os.path.splitext(file)[-1] == '.pdf':
+                pdf_file_num += 1
+                pdf_file_size += os.stat(filepath).st_size
+            else:
+                other_file_num += 1
+                other_file_size += os.stat(filepath).st_size
+
+    print('Number of type .py files is: {:<5d}\ttotal size is {}'.format(py_file_num, sizeConvert(py_file_size)))
+    print(
+        'Number of type .ipynb files is: {:<5d}\ttotal size is {}'.format(ipynb_file_num, sizeConvert(ipynb_file_size)))
+    print('Number of type .exe files is: {:<5d}\ttotal size is {}'.format(exe_file_num, sizeConvert(exe_file_size)))
+    print('Number of type .txt files is: {:<5d}\ttotal size is {}'.format(txt_file_num, sizeConvert(txt_file_size)))
+    print('Number of type .csv files is: {:<5d}\ttotal size is {}'.format(csv_file_num, sizeConvert(csv_file_size)))
+    print('Number of type .pdf files is: {:<5d}\ttotal size is {}'.format(pdf_file_num, sizeConvert(pdf_file_size)))
+    print('Number of other type files is: {:<5d}\ttotal size is {}'.format(other_file_num, sizeConvert(other_file_size)))
 
 
 # MAIN PROGRAM LOGIC
@@ -93,31 +176,33 @@ def main():
     else:
         print('in standard print')
 
-    # todo -d drive info
+    # GET DRIVE INFO
     if args.drv:
-        print('in drv')
+        # print('in drv')
         list_drives(args.drv)
-    else:
-        print('not using drv')
+    # else:
+        # print('not using drv')
 
-    # todo -l folders in drive
+    # GET FOLDER INFO
     if args.fld:
-        print('in fld')
-    else:
-        print('not using fld')
+        # print('in fld')
+        get_folder_info(args.fld)
+    # else:
+    #     print('not using fld')
 
-    # todo -f file name matching
-    # todo is this supposed to accept a file name to search for?
+    # GET FILE INFO
     if args.fil:
-        print('in fil')
-    else:
-        print('not using fil')
+        # print('in fil')
+        get_all_files(args.fil)
+    # else:
+    #     print('not using fil')
 
-    # todo -t all matches of file type
+    # GET FILE TYPE INFO
     if args.typ:
-        print('in typ')
-    else:
-        print('not using typ')
+        # print('in typ')
+        get_all_types(args.typ)
+    # else:
+    #     print('not using typ')
 
 
 if __name__ == '__main__':
